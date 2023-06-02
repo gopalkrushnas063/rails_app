814ce6c6906217afa5a50c00f255e3f1c91987c3
Gopal Krushna Sahoo <gopalk.sahoo@hyscaler.com>
2023-06-02 12:47:20 +0530

    form crated and also give functionality add employe

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,4 +2,35 @@ class EmployeesController < ApplicationController
     def index
         @employees = Employee.all
     end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.new(employee_params)
+        if @employee.save
+            redirect_to employees_path
+          else
+            render :new
+          end
+    end
+    
+    private
+    
+    def employee_params
+        params.require(:employee).permit(
+            :first_name,
+            :middle_name,
+            :last_name,
+            :personal_email,
+            :address_line_1,
+            :address_line_2,
+            :city,
+            :state,
+            :pincode,
+            :country
+        )
+    end
+      
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,9 +1,14 @@
 class Employee < ApplicationRecord
+    validates :first_name, :last_name, presence: true
+    validates :personal_email, presence: true
+    validates :city, :state, :pincode, :country, :address_line_1,  presence: true
+
+
     def name
         "#{first_name} #{middle_name} #{last_name}".strip
     end
 
     def full_address
-        "#{address_line_1}, #{city}, #{state}, #{country}, #{pincode}"
+        "#{address_line_1}, #{city}, #{state}, #{country}, #{pincode}".strip
     end
 end
