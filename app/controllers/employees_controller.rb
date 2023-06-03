bfcb9faad6fec98547e53dccb27cdeff7a0419f1
Gopal Krushna Sahoo <gopalk.sahoo@hyscaler.com>
2023-06-03 11:13:10 +0530

    Dynamically show form for new and edit employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -9,28 +9,35 @@ class EmployeesController < ApplicationController
 
     def create
         @employee = Employee.new(employee_params)
+      
         if @employee.save
-            redirect_to employees_path
-          else
-            render :new
-          end
+          redirect_to employees_path, notice: "Employee has been created successfully"
+        else
+          puts @employee.errors.full_messages # Log error messages to the console
+          render :new
+        end
     end
     
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+
     private
-    
+
     def employee_params
         params.require(:employee).permit(
             :first_name,
-            :middle_name,
             :last_name,
+            :middle_name,
             :personal_email,
-            :address_line_1,
-            :address_line_2,
             :city,
             :state,
+            :country,
             :pincode,
-            :country
+            :address_line_1,
+            :address_line_2
         )
     end
       
 end
+
